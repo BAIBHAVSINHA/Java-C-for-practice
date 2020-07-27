b40f525028f49839cb3474347b1b27a39a1f30f1
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-07-27 18:48:57 +0530

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,14 @@
+class test
+{
+public static void main(string args[])
+{
+byte b=42;
+char c='a';
+short s=1024;
+int i=50000;
+float f=5.67f;
+double d=0.1234;
+double result=(f*b)+(i/c)-(d*c);
+system.out.print(result);
+}
+}
