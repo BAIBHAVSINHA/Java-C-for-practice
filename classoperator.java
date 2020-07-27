c99b9089648dba915295142962ef90e90953e6f9
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-07-27 18:48:26 +0530

    Add files via upload

---
+++ classoperator.java
@@ -0,0 +1,14 @@
+class operator
+{ public static void main(String args[])
+
+{ int a=10,b=-10;
+  boolean c=true,d;
+  System.out.print(~a);
+  System.out.print(~b);
+  System.out.print(!c);
+  System.out.print(!d);
+  b=10;
+  System.out.println(a++ + b++);
+  System.out.println(a++ + ++b);
+}
+}
---
+++ classshiftperator.java
@@ -0,0 +1,10 @@
+class Operator
+{public static void main(String arg[])
+{System.out.println(10<<2);
+ System.out.println(10<<3);
+ System.out.println(20>>2);
+ System.out.println(-20>>2);
+ System.out.println(20>>>2);
+ System.out.println(-1>>>31);
+}
+}
