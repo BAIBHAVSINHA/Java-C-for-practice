be81f1ed41cc85fbaf4b23687e50111d84ba20ec
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-07-28 20:29:07 +0530

    Add files via upload

---
+++ Accessingstaticvariablewithoutobject.java
@@ -0,0 +1,11 @@
+class Accessingstaticvariablewithoutobject
+{
+   static int i = 10;
+   static String s = "Beginnersbook";
+   //This is a static method
+   public static void main(String args[])
+   {
+       System.out.println("i:"+i);
+       System.out.println("s:"+s);
+   }
+}
