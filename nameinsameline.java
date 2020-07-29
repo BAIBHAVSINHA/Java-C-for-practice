b18fee6b14cec9ceffae27aad7a3350cbf495ee4
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-07-29 21:21:26 +0530

    Add files via upload

---
+++ nameinsameline.java
@@ -0,0 +1,10 @@
+class nameinsameline
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        system.out.println("You are "+ name);
+    }
+}
