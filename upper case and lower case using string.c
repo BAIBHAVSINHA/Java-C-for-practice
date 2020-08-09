535726489393b783946e8384a8c3e0ef26bc5a68
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-08-09 22:59:56 +0530

    Add files via upload

---
+++ upper case and lower case using string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   char s[50],c=0;
+   printf("Enter a string to convert it into lower case\n");
+   gets(s);
+   while (s[c] != '\0')
+    {
+      if (s[c] >= 'A' && s[c] <= 'Z')
+      {
+         s[c] = s[c] + 32;
+      }
+      c++;
+    }
+   printf("The string in lower case: %s\n",s);
+   return 0;
+}
