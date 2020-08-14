f59006eb9c2256c485cfb61cddd2bb2bd38c6879
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-08-14 20:07:29 +0530

    Add files via upload

---
+++ read no until -1 is encountered.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,num,count=0,p=0,c=0;
+    scanf("%d",&num);
+    while(num!=-1)
+    {
+
+        for(i=1;i<=n;i++)
+        {
+            if(num%i==0)
+                count++;
+        }
+            if(count==2)
+                p++;
+            else
+                c++;
+        }
+    scanf("%d",&num);
+    }
+    printf("prime no =%d\n",p);
+    printf("composite no =%d\n",c);
+
+}
