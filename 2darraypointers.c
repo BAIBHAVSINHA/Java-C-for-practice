4b3a039a1c49344d62e6ff9080f25d053abd8ebb
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-08-18 22:53:50 +0530

    Add files via upload

---
+++ 2darraypointers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[50][50],i,n,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("\n");
+        for(j=0;j<n;j++)
+        {
+            printf("%d ",*(*(a+i)+j));
+        }
+    }
+}
