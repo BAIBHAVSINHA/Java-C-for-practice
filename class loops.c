6931f45e75305835074f45706383715668144139
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-08-23 22:44:31 +0530

    Add files via upload

---
+++ class loops.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void foo(int **p2)
+{
+    int eq2=5;
+    *p2=&eq2;
+    printf("%d",**p2);
+}
+int fun2()
+{
+    int a=1,b=2,c=3,d=4,e=7;
+    return(a+b);
+}
+int main()
+{
+    int eq=10,*p=eq,s;
+    printf("%d",*p);
+    foo(&p);
+    s=func2();
+    printf("%d",*p);
+    return 0;
+}
