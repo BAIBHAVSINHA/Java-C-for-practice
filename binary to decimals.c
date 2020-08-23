392d826d3566d5167594e9776321cd22ad82891f
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-08-23 22:43:47 +0530

    Add files via upload

---
+++ binary to decimals.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int convertBinaryToDecimal(long long n);
+int main()
+{
+    long long n;
+    printf("Enter a binary number: ");
+    scanf("%d", &n);
+    printf("%d in binary = %d in decimal", n, convertBinaryToDecimal(n));
+    return 0;
+}
+int convertBinaryToDecimal(long long n)
+{
+    int decimalNumber = 0, i = 0, rem;
+    while (n!=0)
+    {
+        rem = n%10;
+        n /= 10;
+        decimalNumber = rem*pow(2,i);
+        ++i;
+    }
+    return decimalNumber;
+}
