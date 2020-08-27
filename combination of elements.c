e7224168935cfd99f8c3868fada9f12ca6d38f49
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-08-27 20:16:55 +0530

    Add files via upload

---
+++ combination of elements.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int combination(int n,int a)
+{
+    i=0;
+    for(j=i+1;j<n;j++)
+    {
+        printf("%d",a[])
+    }
+}
+int main()
+{
+    int n,a[50],r;
+    scanf("%d",&n);
+    fo
+
+}
