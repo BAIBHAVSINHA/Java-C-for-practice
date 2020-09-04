767451423ee6b4632b03e1ce80870b3ff1e8562c
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-04 18:47:15 +0530

    Add files via upload

---
+++ string and function1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void print(char *a)
+{
+    int i=0;
+    while(str[i]!='\0')
+    {
+        printf("%c",str);
+        i++;
+    }
+}
+int main()
+{
+    char str[]="Hello";
+    print(str);
+}
