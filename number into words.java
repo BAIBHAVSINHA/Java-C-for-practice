7eaebff616e6385347458f3164973d7d32e25c14
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-08 19:09:58 +0530

    Update number into words.java

--- number into words.java
+++ number into words.java
@@ -40,3 +40,20 @@ private static final String[] numNames = {
 " Nineteen"
 };
 
+public static void main(String []args)
+{
+Scanner reader = new Scanner(System.in);
+System.out.println("Enter a whole number (max 4 digits): ");
+int n = reader.nextInt();
+System.out.println(n);
+reader.close();
+int units = n%10;
+int tens = (n%100 - units)/10;
+int hundreds = (n%1000 - (tens+units))/100;
+int thousands = (n-(n%1000))/1000;
+System.out.println("In the thousands place: " +thousands);
+System.out.println("In the hundreds place: " +hundreds);
+System.out.println("In the tens place: " +tens);
+System.out.println("In the units place: " +units);
+if(n==0)
+  
