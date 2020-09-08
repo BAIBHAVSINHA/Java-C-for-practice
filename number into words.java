69ce3f0e8fb5bc32d98dfb0c5c67a929ff8465bd
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-08 19:08:35 +0530

    Update number into words.java

--- number into words.java
+++ number into words.java
@@ -1,3 +1,19 @@
 Write a program in java to change number into words ?
 for eg:-> 2341 into Two Thousand Three Hundred Fourthy One
 
+import java.util.Scanner;
+
+public class Num2Txt {
+
+private static final String[] tensNames = {
+"",
+" Ten",
+" Twenty",
+" Thirty",
+" Forty",
+" Fifty",
+" Sixty",
+" Seventy",
+" Eighty",
+" Ninety"
+};
