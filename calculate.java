46e5d52b6b53deab2ceb22c642aec56881374b0e
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-09 19:23:43 +0530

    Update calculate.java

--- calculate.java
+++ calculate.java
@@ -1,6 +1,12 @@
 Q. Write a program that reads from the user four integers representing the numerators and denominators of two fractions,
     calculates the results of the two fractions and displays the values of the fractions sum, subtraction, multiplication and division.
 
-      
+     Sample run:
+          Enter the numerator and denominator of the first fraction: 6 4
+          Enter the numerator and denominator of the second fraction: 8 5
+          The sum is: 3.1
+          The subtraction is: -0.1
+          The multiplication is: 2.4
+          The division is: 0.9375 
       
 Sol:->      
