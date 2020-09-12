9086f0600d036e74ad2ef3d9137dacdeb036a0b9
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-12 21:01:04 +0530

    Update print pattern or character.java

--- print pattern or character.java
+++ print pattern or character.java
@@ -1,4 +1,10 @@
 Write a program to print each line of a file that contains a particular pattern or string oa characters.
 The output should begin with the information on the word and character where this was found(x.y meaning x th word in the line and y thcharacter in the word)
 
-  
+ E.g:
+'ould'
+Input: file.txt
+How could tihs happen?
+Re-mould the nearest output.
+would this always happen?
+ 
