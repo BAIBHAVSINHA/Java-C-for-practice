2bcfdb63af3c27d12da83a90fa552f4410f08c2f
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-12 21:00:33 +0530

    Update print pattern or character.java

--- print pattern or character.java
+++ print pattern or character.java
@@ -1,1 +1,4 @@
+Write a program to print each line of a file that contains a particular pattern or string oa characters.
+The output should begin with the information on the word and character where this was found(x.y meaning x th word in the line and y thcharacter in the word)
 
+  
