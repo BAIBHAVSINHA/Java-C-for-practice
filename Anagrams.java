4497d8263681157e93f0513863ff94d0568c93fa
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-13 21:29:56 +0530

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -6,3 +6,11 @@ public static boolean isAcronym(String s1, String s2) {
     if (s1.length() != s2.length()) return false;
 
     HashMap<Character, Integer> charCounts = new HashMap<>();
+for (int i = 0; i < s1.length(); i++) {
+      if (charCounts.containsKey(s1.charAt(i))) {
+           charCounts.put(s1.charAt(i), charCounts.get(s1.charAt(i)) + 1);
+      } else {
+           charCounts.put(s1.charAt(i), 1);
+      }
+    }
+    
