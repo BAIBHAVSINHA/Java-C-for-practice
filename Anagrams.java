0b8f1e1ab8370d2390f6bd84c15baba6a2ad0b73
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-13 21:33:19 +0530

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -1,28 +1,37 @@
 Write a function to detect if two strings are anagrams ?
   
   Sol:->
-public static boolean isAcronym(String s1, String s2) {
+public static boolean isAcronym(String s1, String s2)
+  {
+      if (s1.length() != s2.length()) return false;
 
-    if (s1.length() != s2.length()) return false;
-
-    HashMap<Character, Integer> charCounts = new HashMap<>();
-for (int i = 0; i < s1.length(); i++) {
-      if (charCounts.containsKey(s1.charAt(i))) {
+      HashMap<Character, Integer> charCounts = new HashMap<>();
+      for (int i = 0; i < s1.length(); i++) 
+      {
+      if (charCounts.containsKey(s1.charAt(i)))
+      {
            charCounts.put(s1.charAt(i), charCounts.get(s1.charAt(i)) + 1);
-      } else {
+      } 
+      else 
+      {
            charCounts.put(s1.charAt(i), 1);
       }
-    }
-    for (int i = 0; i < s2.length(); i++) {
-        if (charCounts.containsKey(s2.charAt(i))) {
+      }
+      for (int i = 0; i < s2.length(); i++)
+      {
+        if (charCounts.containsKey(s2.charAt(i))) 
+        {
            charCounts.put(s2.charAt(i), charCounts.get(s2.charAt(i)) - 1);
-        } else {
+        }
+        else
+        {
            return false;
         }
-    }
-for (int count : charCounts.values()) {
+      }
+      for (int count : charCounts.values())
+      {
         if (count != 0) return false;
-    }
+      }
 
-    return true;
-}
+       return true;
+  }
