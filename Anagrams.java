0dae5f7bfb47d262d4c934b438b124cae2dffeed
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-13 21:29:33 +0530

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -1,3 +1,8 @@
 Write a function to detect if two strings are anagrams ?
   
   Sol:->
+public static boolean isAcronym(String s1, String s2) {
+
+    if (s1.length() != s2.length()) return false;
+
+    HashMap<Character, Integer> charCounts = new HashMap<>();
