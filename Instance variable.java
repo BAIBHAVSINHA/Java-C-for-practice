7fca0deaf0397c052f2f713027ec2ac90d837399
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-15 23:12:17 +0530

    Update Instance variable.java

--- Instance variable.java
+++ Instance variable.java
@@ -1,1 +1,6 @@
+Instance variables are non-static variables and are declared in a class outside any method, constructor or block.
+As instance variables are declared in a class, these variables are created when an object of the class is created and destroyed when the object is destroyed.
+Unlike local variables, we may use access specifiers for instance variables.
+If we do not specify any access specifier then the default access specifier will be used.
 
+  
