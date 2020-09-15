cbb2f7c256c1ae4ba802de32506e029a2f8b1bc3
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-15 23:12:48 +0530

    Update Instance variable.java

--- Instance variable.java
+++ Instance variable.java
@@ -4,3 +4,24 @@ Unlike local variables, we may use access specifiers for instance variables.
 If we do not specify any access specifier then the default access specifier will be used.
 
   
+eg:->
+  public class InstanceVariable { 
+    int instanceVarId; 
+    String instanceVarName; 
+    public static void main(String args[]) 
+    { 
+        InstanceVariable obj = new InstanceVariable(); 
+        obj.instanceVarId = 0001; 
+        obj.instanceVarName = "InstanceVariable1"; 
+        System.out.println("Displaying first Object:"); 
+        System.out.println("instanceVarId==" + obj.instanceVarId); 
+        System.out.println("instanceVarName==" + obj.instanceVarName); 
+  
+        InstanceVariable obj1 = new InstanceVariable(); 
+        obj1.instanceVarId = 0002; 
+        obj1.instanceVarName = "InstanceVariable2"; 
+        System.out.println("Displaying Second Object:"); 
+        System.out.println("instanceVarId==" + obj1.instanceVarId); 
+        System.out.println("instanceVarName==" + obj1.instanceVarName); 
+    } 
+} 
