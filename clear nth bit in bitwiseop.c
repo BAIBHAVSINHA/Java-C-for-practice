2a7399ffe95990051e0ac4f09230eb0fd5052859
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-18 23:52:42 +0530

    Update clear nth bit in bitwiseop.c

--- clear nth bit in bitwiseop.c
+++ clear nth bit in bitwiseop.c
@@ -3,7 +3,6 @@
 int main()
 {
     int num, n, newNum;
-
     /* Input number from user */
     printf("Enter any number: ");
     scanf("%d", &num);
@@ -11,14 +10,12 @@ int main()
     /* Input bit number you want to clear */
     printf("Enter nth bit to clear (0-31): ");
     scanf("%d", &n);
-
     /*
      * Left shifts 1 to n times
      * Perform complement of above
      * finally perform bitwise AND with num and result of above
      */
     newNum = num & (~(1 << n));
-
     printf("Bit cleared successfully.\n\n");
     printf("Number before clearing %d bit: %d (in decimal)\n", n, num);
     printf("Number after clearing %d bit: %d (in decimal)\n", n, newNum);
