e35c0df5fedd2e2d847811d7bebe73b7612a6910
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-20 10:48:24 +0530

    Update to find max element in given array.c

--- to find max element in given array.c
+++ to find max element in given array.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
-
 main()
 {
    int m, n, c, d, matrix[10][10], maximum;
-
    printf("Enter the number of rows and columns of matrix\n");
    scanf("%d%d",&m,&n);
    printf("Enter the elements of matrix\n");
-
    for( c = 0 ; c < m ; c++ )
    {
       for( d = 0 ; d < n ; d++ )
@@ -27,9 +24,7 @@ main()
             maximum = matrix[c][d];
       }
    }
-
    printf("Maximum element in matrix is %d\n", maximum);
-
    getch();
    return 0;
 }
