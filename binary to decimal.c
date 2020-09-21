c0ae07f89ef5ced4699cd0c2d1c3491ebed1a3cc
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-21 21:53:00 +0530

    Update binary to decimal.c

--- binary to decimal.c
+++ binary to decimal.c
@@ -3,21 +3,17 @@ void Binary(int);
 int main()
 {
     int number;
-
     printf("Enter number to convert to binary: ");
     scanf("%d", &number);
-
     Binary(number);
 	return 0;
 }
-
 void Binary(int num)
 {
     if (num == 0)
     {
         return ;
     }
-
     Binary(num / 2);
     printf("%d", num % 2);
 }
