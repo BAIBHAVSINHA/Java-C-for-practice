5ad98f2e91221b9be5b0f8c36042b166eac16c20
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-22 19:22:06 +0530

    Update free memory allocation.c

--- free memory allocation.c
+++ free memory allocation.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
-
     // This pointer will hold the
     // base address of the block created
     int *ptr, *ptr1;
     int n, i, sum = 0;
-
     // Get the number of elements for the array
     n = 5;
     printf("Enter number of elements: %d\n", n);
-
     // Dynamically allocate memory using malloc()
     ptr = (int*)malloc(n * sizeof(int));
-
     // Dynamically allocate memory using calloc()
     ptr1 = (int*)calloc(n, sizeof(int));
-
     // Check if the memory has been successfully
     // allocated by malloc or not
     if (ptr == NULL || ptr1 == NULL) {
@@ -41,6 +35,5 @@ int main()
         free(ptr1);
         printf("Calloc Memory successfully freed.\n");
     }
-
     return 0;
 }
