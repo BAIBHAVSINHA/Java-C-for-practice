6460126e1033a200452c128dfbb7d52f0a7c8a3a
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2020-10-05 23:13:42 +0530

    Update armstrongno.java

--- armstrongno.java
+++ armstrongno.java
@@ -1,9 +1,7 @@
 public class armstrongno{
-
     public static void main(String[] args) {
 
         int num = 370, number, temp, total = 0;
-
         number = num;
         while (number != 0)
         {
@@ -11,7 +9,6 @@ public class armstrongno{
             total = total + temp*temp*temp;
             number /= 10;
         }
-
         if(total == num)
             System.out.println(num + " is an Armstrong number");
         else
