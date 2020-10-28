4453306e0b6115e83aa7d0aefb8eeb926f0aaa11
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2020-10-28 17:42:14 +0530

    Update class loops.c

--- class loops.c
+++ class loops.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
+
 void foo(int **p2)
 {
     int eq2=5;
     *p2=&eq2;
     printf("%d",**p2);
 }
+
 int fun2()
 {
     int a=1,b=2,c=3,d=4,e=7;
     return(a+b);
 }
+
 int main()
 {
     int eq=10,*p=eq,s;
