6dd73504128f584d01456aa296ec2ec1b3be7887
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2020-11-03 21:10:44 +0530

    Update FizzBuzz problem.java

--- FizzBuzz problem.java
+++ FizzBuzz problem.java
@@ -2,7 +2,6 @@ Q. Write a Java program that prints the numbers from 1 to 50. But for multiples 
    instead of the number and for the multiples of five print "Buzz".
    For numbers which are multiples of both three and five print "FizzBuzz" ?
 Sol:->
- 
       public class FizzBuzzTest
            {  
              public static void main(String args[])
