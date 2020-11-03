28f36f350f55c5233e2eca5969cdaf604781fbb0
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2020-11-03 21:13:39 +0530

    Update binary equivalence using recursion.c

--- binary equivalence using recursion.c
+++ binary equivalence using recursion.c
@@ -4,20 +4,20 @@ Sol:->
     int binary(int);
     int main()
     {
-    int num, bin;
-    printf("Enter a decimal number: ");
-    scanf("%d", &num);
-    bin = binary(num);
-    printf("The binary equivalent of %d is %d\n", num, bin);
+        int num, bin;
+        printf("Enter a decimal number: ");
+        scanf("%d", &num);
+        bin = binary(num);
+        printf("The binary equivalent of %d is %d\n", num, bin);
     }
     int binary(int num)
     {
-    if (num == 0)
-    {
+        if (num == 0)
+        {
         return 0;
-    }
-    else
-    {
+        }
+        else
+        {
         return (num % 2) + 10 * binary(num / 2);
-    }
+        }
     }
